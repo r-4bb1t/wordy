736aab5ca61537e57cab4b82009d80b186f6dbf3
r4bb1t <r4bb1t.github@gmail.com>
2023-12-14 10:03:25 +0900

    feat: add translator

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import Translate from "./components/translate";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Translate />
+    </main>
   );
 }
