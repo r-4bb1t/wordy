77544759ac2dcb5d64b2e596010f27895bef4fdd
r4bb1t <r4bb1t.github@gmail.com>
2024-01-18 14:58:27 +0900

    style: mobile word font size

--- app/article/[id]/words.tsx
+++ app/article/[id]/words.tsx
@@ -8,7 +8,7 @@ export default function Words({ words }: { words: WordType[] }) {
           key={i}
           className="grid md:grid-cols-[200px_1fr] break-inside-avoid"
         >
-          <div className="font-black text-primary text-xl flex md:items-center px-4 py-2 gap-4 border-t-4 border-t-primary !bg-base-200">
+          <div className="font-black text-primary text-base md:text-lg lg:text-xl flex md:items-center px-4 py-2 gap-4 border-t-4 border-t-primary !bg-base-200">
             {word.word}
             <div className="font-medium md:hidden">
               <span className="text-primary">
@@ -17,17 +17,19 @@ export default function Words({ words }: { words: WordType[] }) {
               {word.meaning.split(". ")[1]}
             </div>
           </div>
-          <div className="flex flex-col gap-1 px-4 border-t border-t-base-300 py-4 md:py-2">
+          <div className="flex flex-col gap-1 px-4 border-t border-t-base-300 py-3 md:py-2">
             <div className="font-medium hidden md:block">
               <span className="text-primary">
                 {word.meaning.split(". ")[0] + ". "}
               </span>
               {word.meaning.split(". ")[1]}
             </div>
-            <div className="text-xs font-medium">
+            <div className="text-sm md:text-xs font-medium">
               {word.exampleSentence.sentence}
             </div>
-            <div className="text-xs">{word.exampleSentence.meaning}</div>
+            <div className="text-sm md:text-xs text-black/80">
+              {word.exampleSentence.meaning}
+            </div>
           </div>
         </li>
       ))}
