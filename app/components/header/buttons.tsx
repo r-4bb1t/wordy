c9b1990ad8e0cf6c7081a809c505ac2d97ec4079
r4bb1t <r4bb1t.github@gmail.com>
2024-01-18 19:01:12 +0900

    refactor: fix greeting text

--- app/components/header/buttons.tsx
+++ app/components/header/buttons.tsx
@@ -1,27 +1,26 @@
 "use client";
 
 import { User } from "next-auth";
-import { useMemo, useState } from "react";
+import { useState } from "react";
 import { IoPersonCircleOutline } from "react-icons/io5";
 import SignIn from "../sign-in";
 import Modal from "../modal";
 
+const greet = () => {
+  const hour = new Date().getHours();
+  if (hour >= 4 && hour <= 11) return "Good Morning";
+  if (hour >= 12 && hour <= 17) return "Good Afternoon";
+  return "Good Evening";
+};
+
 export default function HeaderButtons({ user }: { user: User | undefined }) {
   const [isOpenedSignIn, setIsOpenedSignIn] = useState(false);
-
-  // good morning or good evening or good afternoon
-  const greet = useMemo(() => {
-    const hour = new Date().getHours();
-    if (hour >= 4 && hour <= 11) return "Good Morning";
-    if (hour >= 12 && hour <= 17) return "Good Afternoon";
-    return "Good Evening";
-  }, []);
 
   return (
     <>
       {user ? (
         <button className="btn btn-ghost normal-case font-medium gap-0 pointer-events-none">
-          {greet},<span className="ml-1 !font-black">{user.name}</span>
+          {greet()},<span className="ml-1 !font-black">{user.name}</span>
           <div className="w-6 h-6 overflow-hidden rounded-full ml-2">
             {user.image ? (
               <img
