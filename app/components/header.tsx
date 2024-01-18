1afe6623e46520c41c622754b13728c2547d1b62
r4bb1t <r4bb1t.github@gmail.com>
2024-01-18 14:40:52 +0900

    style: disable header in print mode

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="fixed top-0 w-full flex items-center py-4 justify-between px-6 md:px-12 bg-black z-10">
+    <header className="fixed top-0 w-full flex items-center py-4 justify-between px-6 md:px-12 bg-black z-10 print:hidden">
       <Link href={"/"} className="text-white font-black">
         wordy
       </Link>
