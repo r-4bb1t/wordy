1184b5e0d4f3b1d61c4cc6b14a19748661816020
r4bb1t <r4bb1t.github@gmail.com>
2024-01-20 18:04:19 +0900

    fix: update emphasize utility not to emphasize word in md link

--- app/utils/emphasize.ts
+++ app/utils/emphasize.ts
@@ -2,23 +2,24 @@ import { WordType } from "../types/result";
 import { load } from "cheerio";
 
 export const emphasize = (text: string, words: WordType[]) => {
-  const regex = new RegExp(
-    `(${words.map((word) => word.word).join("|")})`,
+  const wordRegex = new RegExp(
+    `(?<!\\[.*?)(${words.map((word) => word.word).join("|")})(?!.*?\\])`,
     "gi"
   );
-  const t = text.replace(
-    regex,
-    `<span class="highlight"><span class="ttip">$1</span>$1</span>`
-  );
+
+  const t = text.replace(wordRegex, (match) => {
+    return `<span class="highlight"><span class="ttip">${match}</span>${match}</span>`;
+  });
+
   // find all the words in the text and append the tooltip
   const $ = load(t);
   words.forEach((word) => {
     const firstLetterUpperCase =
       word.word[0].toUpperCase() + word.word.slice(1);
-    $(`span.highlight:contains(${word.word})`).each((i, elem) => {
+    $(`span.highlight:contains(${word.word})`).each((_, elem) => {
       $(elem).find(".ttip").text(word.meaning);
     });
-    $(`span.highlight:contains(${firstLetterUpperCase})`).each((i, elem) => {
+    $(`span.highlight:contains(${firstLetterUpperCase})`).each((_, elem) => {
       $(elem).find(".ttip").text(word.meaning);
     });
   });
