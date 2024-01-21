07e6d6c4ff51da75c49327f9ae5202ed8da88130
r4bb1t <r4bb1t.github@gmail.com>
2024-01-21 14:57:19 +0900

    fix: example sentence bug

--- app/article/[id]/words.tsx
+++ app/article/[id]/words.tsx
@@ -4,8 +4,10 @@ import Modal from "@/app/components/modal";
 import SignIn from "@/app/components/sign-in";
 import { useUserStore } from "@/app/store/user-store";
 import { WordType } from "@/app/types/result";
-import { ChangeEvent, useState } from "react";
+import { UserType } from "@/app/types/user";
+import { Dispatch, useState } from "react";
 import { IoChevronDown, IoHeart, IoHeartOutline } from "react-icons/io5";
+import cc from "classcat";
 
 export default function Words({ words }: { words: WordType[] }) {
   const { user } = useUserStore();
@@ -26,55 +28,13 @@ export default function Words({ words }: { words: WordType[] }) {
     <>
       <ul className="w-full text-sm bg-base-100 grid md:grid-cols-2">
         {words.map((word, i) => (
-          <li key={i}>
-            <label className="flex flex-col break-inside-avoid divide-y cursor-pointer md:cursor-auto">
-              <div className="flex justify-between w-full px-4 py-2 items-center gap-4">
-                <label
-                  className="hover:scale-110 transition-transform swap"
-                  onClick={() => {
-                    if (!user) {
-                      setIsOpenedSignIn(true);
-                      return;
-                    }
-                  }}
-                >
-                  <input
-                    type="checkbox"
-                    className="hidden"
-                    id={`word-${i}`}
-                    defaultChecked={word.isLiked}
-                    disabled={!user}
-                    onChange={() => {
-                      handleLike(word);
-                    }}
-                  />
-                  <IoHeart size={20} className="fill-primary swap-on" />
-                  <IoHeartOutline size={20} className="fill-primary swap-off" />
-                </label>
-                <div className="w-full font-black text-primary text-base flex flex-col md:flex-row md:gap-4">
-                  <div>{word.word}</div>
-                  <div className="font-medium text-base">
-                    <span className="text-primary">
-                      {word.meaning.split(". ")[0] + ". "}
-                    </span>
-                    {word.meaning.split(". ")[1]}
-                  </div>
-                </div>
-                <div className="md:hidden">
-                  <IoChevronDown />
-                </div>
-              </div>
-              <input className="peer hidden" type="checkbox" id={`word-${i}`} />
-              <div className="border-t border-t-base-300 justify-center overflow-hidden h-full max-h-0 peer-checked:max-h-[500px] md:!max-h-[500px] transition-all">
-                {word.exampleSentence.map((e, i) => (
-                  <div className="flex flex-col gap-1 py-3 px-4" key={i}>
-                    <div className="font-medium">{e.sentence}</div>
-                    <div className="opacity-80">{e.meaning}</div>
-                  </div>
-                ))}
-              </div>
-            </label>
-          </li>
+          <Word
+            word={word}
+            user={user}
+            setIsOpenedSignIn={setIsOpenedSignIn}
+            handleLike={handleLike}
+            key={i}
+          />
         ))}
       </ul>
       <Modal opened={isOpenedSignIn} close={() => setIsOpenedSignIn(false)}>
@@ -83,3 +43,80 @@ export default function Words({ words }: { words: WordType[] }) {
     </>
   );
 }
+
+const Word = ({
+  word,
+  user,
+  setIsOpenedSignIn,
+  handleLike,
+}: {
+  word: WordType;
+  user: UserType | null;
+  setIsOpenedSignIn: Dispatch<React.SetStateAction<boolean>>;
+  handleLike: (word: WordType) => void;
+}) => {
+  const [isOpened, setIsOpened] = useState(false);
+  return (
+    <li>
+      <div className="flex flex-col break-inside-avoid divide-y cursor-pointer md:cursor-auto">
+        <div className="flex justify-between w-full px-4 py-2 items-center gap-4">
+          <div
+            className="hover:scale-110 transition-transform swap"
+            onClick={() => {
+              if (!user) {
+                setIsOpenedSignIn(true);
+                return;
+              }
+            }}
+          >
+            <input
+              type="checkbox"
+              className="hidden"
+              id={`word-${word.word}`}
+              defaultChecked={word.isLiked}
+              disabled={!user}
+              onChange={() => {
+                handleLike(word);
+              }}
+            />
+            <IoHeart size={20} className="fill-primary swap-on" />
+            <IoHeartOutline size={20} className="fill-primary swap-off" />
+          </div>
+          <div
+            className="w-full font-black text-primary text-base flex flex-col md:flex-row md:gap-4"
+            onClick={() => setIsOpened((s) => !s)}
+          >
+            <div>{word.word}</div>
+            <div className="font-medium text-base">
+              <span className="text-primary">
+                {word.meaning.split(". ")[0] + ". "}
+              </span>
+              {word.meaning.split(". ")[1]}
+            </div>
+          </div>
+          <div
+            className={cc([
+              "md:hidden transition-transform",
+              isOpened && "rotate-180",
+            ])}
+          >
+            <IoChevronDown />
+          </div>
+        </div>
+        <div
+          className={cc([
+            "border-t border-t-base-300 justify-center overflow-hidden h-full max-h-0 md:!max-h-[500px] transition-all",
+            isOpened && "max-h-[500px]",
+          ])}
+        >
+          {word.exampleSentence.map((e, i) => (
+            <div className="flex flex-col gap-1 py-3 px-4" key={i}>
+              <div className="font-medium">{e.sentence}</div>
+              <div className="opacity-80">{e.meaning}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </li>
+  );
+};
